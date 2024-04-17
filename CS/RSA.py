303a0e0aa69394a5795a57f5425175a83f5c84ec
fayrouz41 <FS2200869@tkh.edu.eg>
2024-04-17 14:26:19 +0200

    Update RSA.py

--- CS/RSA.py
+++ CS/RSA.py
@@ -1,6 +1,29 @@
 import math 
 p= int (input("enter a prime number"))
 q= int (input("enter a prime number"))
+def prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+if prime(p): 
+    print (True)
+else:
+    print (f"{p} is not prime")
+if prime(q): 
+    print (True)
+else:
+    print (f"{q} is not prime")
+
+
 n = p*q
 eul= (p-1)*(q-1)
 # e=3 
@@ -9,20 +32,6 @@ eul= (p-1)*(q-1)
 #     while b != 0:
 #         a ,b = b, a%b
 #     return a 
-while e < eul:
-    if gcd(e,eul)==1:
-        break
-    else: 
-        e+=1
-d=pow(e,-1,eul)
-print(f"public:{n,e}")
-print(f"private:{n,d}")
-m=11
-c=pow(m,e,n)
-M=pow(c,d,n)
-print(m,c,M)
-
-
 def extended_gcd(a,b): 
     x0,x1,y0,y1 = 1, 0, 0, 1
     while b: 
@@ -35,10 +44,22 @@ e=17
 eul=40
 gcd, x, y =extended_gcd(e,eul)
 
-def calculate_private_exponent(e, phi_N):
-    gcd, x, _ = extended_gcd(e, phi_N)
-    if gcd != 1:
-        raise ValueError("Public exponent and totient are not coprime.")
-    return x % phi_N = x
+def calculate_private_exponent(e, ):
 
-print(f"The private exponent (d) is:{private_exponent}")
+
+
+# ..
+while e < eul:
+    if extended_gcd(e,eul)==1:
+        break
+    else: 
+        e+=1
+d=pow(e,-1,eul)
+print(f"public:{n,e}")
+print(f"private:{n,d}")
+m=11
+c=pow(m,e,n)
+M=pow(c,d,n)
+print(m,c,M)
+
+   
